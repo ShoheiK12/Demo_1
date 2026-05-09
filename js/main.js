dc963c9be75052354437f1e6dafec53d0b0a31e4
ShoheiK12 <shohei.kotera22@gmail.com>
2026-05-09 11:06:20 +1000

    Add contact page and Improve other pages related to contact page

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
-// $(function() {}); HTMLの読み込み完了後に実行する
-// -> HTMLが全部読み込まれてからSwiperを作る
-// -> JavaScriptは上から順に実行される。現在nain.jsはindex.htmlで<head>にある。ブラウザは、1. main.jsを読む　2. まだ<body>は未読　3. .swiper が存在しない　4. Swiper生成失敗になる。しかし、$(function() {});を使うと、1. HTML全部読む　2. bodyも完成　3. .swiper が存在　4. Swiper生成成功になる。
-$(function(){
+document.addEventListener('DOMContentLoaded', () => {
 
   /*=================================================
   ハンバーガーメニュー
   ===================================================*/
-  $('.toggle_btn').on('click', function() {
+  const header = document.getElementById('header');
+  const toggleBtn = document.querySelector('.toggle_btn');
+  const mask = document.getElementById('mask');
 
-    if ($('#header').hasClass('open')) {
-
-      $('#header').removeClass('open');
-
-    } else {
-
-      $('#header').addClass('open');
-    }
+  // メニュー開閉
+  toggleBtn.addEventListener('click', () => {
+    header.classList.toggle('open');
   });
 
-  $('#mask').on('click', function() {
-
-    $('#header').removeClass('open');
+  // 背景クリックで閉じる
+  mask.addEventListener('click', () => {
+    header.classList.remove('open');
   });
 
 
@@ -32,8 +26,6 @@ $(function(){
     loop: true,
 
     speed: 1000,
-    
-    slidesPerView: 1,
 
     autoplay: {
       delay: 3000,
